91ebcdf0d2841e81414b5b6bb0a946f2fdc52729
yancj9ya <86911331+yancj9ya@users.noreply.github.com>
2026-03-26 23:22:51 +0800

    chore(config): read port from env and update docker-compose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    port = int(os.environ.get("PORT", "8000"))
+    app.run(host="0.0.0.0", port=port, debug=True)
